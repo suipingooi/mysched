00baf65aebb3b774b668ba7a770be120be3a8646
SuiPing <kam.suipingooi@me.com>
2021-03-11 11:36:45 +0000

    added students CR route

--- iifscDB/app.py
+++ iifscDB/app.py
@@ -66,6 +66,35 @@ def students_list():
                            skaters=skaters)
 
 
+# students new - CREATE
+@app.route('/students/add')
+def newstudent_form():
+    return render_template('form_newstudent.template.html')
+
+
+@app.route('/students/add', methods=["POST"])
+def add_newstudent():
+    student_fname = request.form.get('student_fname')
+    student_lname = request.form.get('student_lname')
+    date_of_birth = request.form.get('date_of_birth')
+    citizenship = request.form.get('citizenship')
+    student_email = request.form.get('student_email')
+    student_phone = request.form.get('student_phone')
+    skate_level = request.form.get('skate_level')
+
+    db.students.insert_one({
+        "student_fname": student_fname,
+        "student_lname": student_lname,
+        "date_of_birth": date_of_birth,
+        "citizenship": citizenship,
+        "student_email": student_email,
+        "student_phone": student_phone,
+        "skate_level": skate_level
+    })
+
+    return "New Student Added"
+
+
 # rinks listing - READ
 @app.route('/rinks')
 def rinks_list():
